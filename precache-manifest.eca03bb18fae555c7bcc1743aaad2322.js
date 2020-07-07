3629f56d1d0a98ba1dae96e58a0c4f48b9d3174d
oscarel <oscar.kemuel5@gmail.com>
2020-07-06 23:10:56 -0300

    Updates

---
+++ precache-manifest.eca03bb18fae555c7bcc1743aaad2322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74214cb2ef5a757dca91ade48d89a007",
+    "url": "/react-interface-facebook/index.html"
+  },
+  {
+    "revision": "8d9e60d36a337d223e9e",
+    "url": "/react-interface-facebook/static/css/main.0123cc18.chunk.css"
+  },
+  {
+    "revision": "041218bf8e277f62b77a",
+    "url": "/react-interface-facebook/static/js/2.0f3d4cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-interface-facebook/static/js/2.0f3d4cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9e60d36a337d223e9e",
+    "url": "/react-interface-facebook/static/js/main.ba57ae1f.chunk.js"
+  },
+  {
+    "revision": "b312d4bc92e8d26e3438",
+    "url": "/react-interface-facebook/static/js/runtime-main.c8d8f288.js"
+  }
+]);
